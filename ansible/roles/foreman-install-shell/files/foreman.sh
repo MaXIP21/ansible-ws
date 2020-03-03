fc27f364fc6ec80aca4ca7cebc6da07a41d70091
root <root@ansible-dev.cw01.contiwan.com>
2020-03-03 06:51:35 -0600

    Created foreman conugiration and answers template

--- ansible/roles/foreman-install-shell/files/foreman.sh
+++ ansible/roles/foreman-install-shell/files/foreman.sh
@@ -4,9 +4,9 @@ hostnamectl set-hostname foreman.example.com
 echo "$_IP foreman.example.com" | sudo tee -a /etc/hosts
 sudo apt -y update
 sudo apt -y install ca-certificates wget
-wget https://apt.puppetlabs.com/puppet-release-bionic.deb
-sudo dpkg -i puppet-release-bionic.deb
-rm puppet-release-bionic.deb
+#wget https://apt.puppetlabs.com/puppet-release-bionic.deb
+#sudo dpkg -i puppet-release-bionic.deb
+#rm puppet-release-bionic.deb
 echo "deb http://deb.theforeman.org/ bionic 1.23" | sudo tee /etc/apt/sources.list.d/foreman.list
 echo "deb http://deb.theforeman.org/ plugins 1.23" | sudo tee -a /etc/apt/sources.list.d/foreman.list
 sudo apt -y install ca-certificates
